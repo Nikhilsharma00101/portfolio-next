6197bb77d1fdb3149741de35dd681eb2e5cbdebf
unknown <nikhil18981@gmail.com>
2025-09-21 00:48:00 +0530

    feat : created Footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,60 @@
 import Navbar from "@/components/navbar";
 import Hero from "@/components/herosection/hero";
-import DraggableCards  from "@/components/aboutsection/AboutSection";
-import OrbitingSkills  from "@/components/skillssection/skills";
+import DraggableCards from "@/components/aboutsection/AboutSection";
+import OrbitingSkills from "@/components/skillssection/skills";
 import ProjectCarousel from "@/components/projectsection/project";
 import TimelineGalaxy from "@/components/projectplanning/TimelineGalaxy";
 import Testimonials from "@/components/testimonials/Testimonials";
 import CertificateCarousel from "@/components/certificates/certificates";
 import Contact from "@/components/contactsection/contact";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black text-white">
+    <main className="min-h-screen bg-black text-white scroll-smooth">
       {/* Navbar */}
       <Navbar />
+
       {/* Hero Section */}
       <Hero />
+
       {/* About Section */}
-      <DraggableCards />
+      <section id="about">
+        <DraggableCards />
+      </section>
+
       {/* Skills Section */}
-      <OrbitingSkills />
+      <section id="skills">
+        <OrbitingSkills />
+      </section>
+
       {/* Projects Section */}
-      <ProjectCarousel />
+      <section id="projects">
+        <ProjectCarousel />
+      </section>
+
       {/* Project Planning Section */}
-      <TimelineGalaxy />
+      <section id="planning">
+        <TimelineGalaxy />
+      </section>
+
       {/* Testimonials Section */}
-      <Testimonials />
+      <section id="testimonials">
+        <Testimonials />
+      </section>
+
       {/* Certificate Section */}
-      <CertificateCarousel/>
-      {/* contact section */}
-      <Contact/>
+      <section id="certificates">
+        <CertificateCarousel />
+      </section>
+
+      {/* Contact Section */}
+      <section id="contact">
+        <Contact />
+      </section>
+
+      {/* Footer */}
+      <Footer />
     </main>
   );
 }
