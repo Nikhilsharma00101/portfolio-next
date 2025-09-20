3c992f6cf51da2a19e55100a59876b3194a03e74
unknown <nikhil18981@gmail.com>
2025-09-20 18:01:36 +0530

    feat : created testimonials section and added 3 testimonials in it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import DraggableCards  from "@/components/aboutsection/AboutSection";
 import OrbitingSkills  from "@/components/skillssection/skills";
 import ProjectCarousel from "@/components/projectsection/project";
 import TimelineGalaxy from "@/components/projectplanning/TimelineGalaxy";
+import Testimonials from "@/components/testimonials/Testimonials";
 
 export default function Home() {
   return (
@@ -20,6 +21,8 @@ export default function Home() {
       <ProjectCarousel />
       {/* Project Planning Section */}
       <TimelineGalaxy />
+      {/* Testimonials Section */}
+      <Testimonials />
     </main>
   );
 }
